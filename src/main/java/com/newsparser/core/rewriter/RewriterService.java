181015ef3f2d15e5cc32a78214f68a2ee90082d2
Max <mnnvh@yandex.ru>
2022-05-30 12:00:54 +0300

    trash

--- src/main/java/com/newsparser/core/rewriter/RewriterService.java
+++ src/main/java/com/newsparser/core/rewriter/RewriterService.java
@@ -1,12 +1,6 @@
 package com.newsparser.core.rewriter;
 
-
-
-
 import org.springframework.http.*;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.http.codec.xml.Jaxb2XmlDecoder;
-import org.springframework.http.codec.xml.Jaxb2XmlEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -14,15 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.net.URI;
-//import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
+
 
 @Service
 public class RewriterService {
